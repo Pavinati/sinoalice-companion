d105cfb79e703c9b40a663b33637ec35c8dfb235
Mattia <mattia.pavinati@ispirata.com>
2021-01-05 00:34:51 +0100

    Fix broken sinoalice link
    Add protocol to the sinoalice-db link
    
    Signed-off-by: Mattia <mattia.pavinati@ispirata.com>

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -25,7 +25,7 @@ const AboutPage = () => (
     <p>For any bug, question or feedback, please open an issue on <a href="https://github.com/Pavinati/sinoalice-companion" target="_blank" rel="noreferrer">GitHub</a></p>
 
     <h4>Credits</h4>
-    <p>Thanks to <a href="sinoalice.game-db.tw" target="_blank" rel="noreferrer">sinoalice db</a> for the weapon images and stats.</p>
+    <p>Thanks to <a href="https://sinoalice.game-db.tw" target="_blank" rel="noreferrer">sinoalice db</a> for the weapon images and stats.</p>
     <p>A big thanks to Blue#7239 and the crew at #en-sino-number-crunching for the precious feedback and spreadsheets formulae.</p>
   </Box>
 );
