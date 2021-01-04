1531ffdb0a1e1c4c012c4ef0e2632cd31668f733
Mattia <mattia.pavinati@ispirata.com>
2021-01-04 18:44:59 +0100

    Prepare for the first beta release

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -1,13 +1,31 @@
+/*
+SINoALICE companion
+Copyright (C) 2021 Pavij
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 import Box from '@material-ui/core/Box';
 
 const AboutPage = () => (
   <Box>
     <p>Made by Pavij (Pavij#9569)</p>
-    <p>If you like what i do and you want to contribute or support my work, feel free to join me on <a href="https://www.patreon.com/pavij" target="_blank">Patreon</a></p>
-    <p>For any bug, question or feedback, please open an issue on <a href="#" target="_blank">GitHub</a></p>
+    <p>If you like what i do and you want to contribute or support my work, feel free to join me on <a href="https://www.patreon.com/pavij" target="_blank" rel="noreferrer">Patreon</a></p>
+    <p>For any bug, question or feedback, please open an issue on <a href="https://github.com/Pavinati/sinoalice-companion" target="_blank" rel="noreferrer">GitHub</a></p>
 
     <h4>Credits</h4>
-    <p>Thanks to <a href="sinoalice.game-db.tw" target="_blank">sinoalice db</a> for the weapon images and stats.</p>
+    <p>Thanks to <a href="sinoalice.game-db.tw" target="_blank" rel="noreferrer">sinoalice db</a> for the weapon images and stats.</p>
     <p>A big thanks to Blue#7239 and the crew at #en-sino-number-crunching for the precious feedback and spreadsheets formulae.</p>
   </Box>
 );
--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
+/*
+SINoALICE companion
+Copyright (C) 2021 Pavij
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 import './App.css';
 
 import clsx from 'clsx';
@@ -11,6 +29,7 @@ import {
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 import AppBar from '@material-ui/core/AppBar';
+import Box from '@material-ui/core/Box';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import IconButton from '@material-ui/core/IconButton';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -133,7 +152,10 @@ const App = () => {
             >
               <MenuIcon />
             </IconButton>
-            <Typography variant="h5">Pavij&apos;s build optimizer</Typography>
+            <Typography variant="h5">Pavij&apos;s SINoALICE companion</Typography>
+            <Box ml={1}>
+              <Typography variant="small">v0.1 (BETA)</Typography>
+            </Box>
           </Toolbar>
         </AppBar>
         <Menu
